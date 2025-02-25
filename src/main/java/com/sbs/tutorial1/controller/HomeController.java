1400d43688ba24b835c7a652bfabb704e89d2adb
YeongKing <whdcks208@naver.com>
2025-02-25 22:22:02 +0900

    HomeController

---
+++ src/main/java/com/sbs/tutorial1/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.sbs.tutorial1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+// 개발자가 스프링부트에게 말한다.
+// 이 클래스는 웹 요청을 받아서 작업을 한다.
+// 해당 클래스는 컨트롤러야!
+public class HomeController {
+    @GetMapping("/home/main")
+    // 개발자가 /home/main 이르는 요청을 보내면 아래 메서드를 실행
+    @ResponseBody
+    // 아래 메서드를 실행 후 리턴값을 응답으로 삼아
+    // body에 출력해줘
+    public String showHome(){
+        return "어서오세요.";
+
+    }
+}
